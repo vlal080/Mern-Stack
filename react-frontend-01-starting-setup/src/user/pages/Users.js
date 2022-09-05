1ca63e7332867cba018c7c8bf82b5ca5b1eccb2c
Vanshika <85989897+Van-ss@users.noreply.github.com>
2022-09-05 16:55:05 +1200

    -Setup

---
+++ react-frontend-01-starting-setup/src/user/pages/Users.js
@@ -0,0 +1,18 @@
+import React from "react";
+import UsersList from "../components/UsersList";
+
+
+const Users = () => {
+    const USERS = [
+    {
+    id: 'u1', 
+    name: 'Max Schwarz', 
+    image: 'https://www.bing.com/images/search?view=detailV2&ccid=r3dKdC%2bI&id=5972880C03E062B0ADB34F656F7C327450D133DD&thid=OIP.r3dKdC-IHYHwP-wm9ldIIQHaHa&mediaurl=https%3a%2f%2fth.bing.com%2fth%2fid%2fR.af774a742f881d81f03fec26f6574821%3frik%3d3TPRUHQyfG9lTw%26riu%3dhttp%253a%252f%252fpre00.deviantart.net%252f9024%252fth%252fpre%252ff%252f2015%252f162%252f4%252f5%252fprofile_pic_animation_by_incognitobserver-d8wwyu7.jpg%26ehk%3dSDXvV5uXShxOQWaQQqye0oT3Q%252fmOU%252fMGvCVT%252fZzsR88%253d%26risl%3d%26pid%3dImgRaw%26r%3d0&exph=894&expw=894&q=profile+animated+pic+&simid=608014073608153724&FORM=IRPRST&ck=49991D65C896C960C4F02AADE6823795&selectedIndex=0&ajaxhist=0&ajaxserp=0',
+    places:3}
+];
+    return (
+        <UsersList items={}/>
+    );
+};
+
+export default Users;
